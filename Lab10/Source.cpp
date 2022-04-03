5358ead278562941956c738b31ade8ea24e12866
Igor Semikin <i.semikin@list.ru>
2022-04-03 15:17:58 +0300

    Доделал 1 и 19 задание

--- Lab10/Source.cpp
+++ Lab10/Source.cpp
@@ -7,19 +7,59 @@
 
 int askUserNumberTask()
 {
-	int choise;
+	int choice;
 	do {
-		printf("Введите номер задания (1, 10, 19):\nВыбор:"); scanf_s("%d", &choise);
-	} while (choise != 1 && choise != 10 && choise != 19);
-	return choise;
+		printf("Введите номер задания (1, 10, 19):\nВыбор:"); scanf_s("%d", &choice);
+	} while (choice != 1 && choice != 10 && choice != 19);
+	return choice;
 }
 
-char* choiseLetter()
+char choiceLetter()
 {
-	char let[1];
+	char let;
 	printf("Введите букву:\n");
-	scanf_s("%c", &let[0]);
+	scanf_s("%c", &let);
 	return let;
+}
+void letterMet(char* str, char let)
+{
+	int counter = 0;
+	for (int i = 0; i < strlen(str); i++)
+		if (str[i] == let)
+			counter++;
+	printf("Буква '%c' встретилась %d раз(а).", let, counter);
+}
+
+void recurringSymbol(char* wrd1, char* wrd2, char* c)
+{
+	for (int i = 0; wrd1[i] != '\0'; i++)
+	{
+		int counter = 0;
+		for (int j = 0; wrd1[j] != '\0'; j++)
+			if (wrd1[i] == wrd1[j])
+				counter++;
+		if (counter == 1)
+		{
+			int counter2 = 0;
+			for (int k = 0; wrd2[k] != '\0'; k++)
+				if (wrd1[i] == wrd2[k])
+				{
+					counter2++;
+					c[i] = wrd1[i];
+				}
+			if (counter2 == 1)
+				printf("%c\n", c[i]);
+		}
+	}
+}
+
+bool chekPalindrom(char* mas)
+{
+	int len = strlen(mas);
+	for (int i = 0; i < len / 2; i++)
+		if (mas[i] != mas[len - i - 1])
+			return false;
+	return true;
 }
 
 int main()
@@ -29,58 +69,36 @@ int main()
 	#pragma region 1 ЗАДАНИЕ
 	char str[256];
 	printf("Введите строку:\n"); gets_s(str);
-	char *let = choiseLetter();
+	char let = choiceLetter();
 
-	int counter = 0;
-	for (int i = 0; i < strlen(str); i++)
-		if (str[i] == *let)
-			counter++;
-	printf("Буква '%c' встретилась %d раз(а).", *let, counter);
+	letterMet(str, let);
 	#pragma endregion
 
 	#pragma region 10 ЗАДАНИЕ
-	//char str[256];
-	//printf("Введите слово:\n"); gets_s(str);
+	/*char str[256];
+	printf("Введите строку:\n"); gets_s(str);
 
-	//char mas[50];
-	//for (int i = 0; str[i] != '\0'; i++)
-	//{
-	//	if (str[i] == ' ')
-	//	{
-	//		mas[i] = str[i];
-	//		int len = strlen(mas);
-	//		for (int j = i; j <len/2; j++)
-	//		{
-	//			if (len % 2 == 0)
-	//			{
-	//				if (mas[j] != mas[len - j - 1])// палиндром
-	//					printf("Да");
-	//			}
-	//			else
-	//				printf("Нет");
-	//		}
-	//	}
-	//}
+	char mas[50];
+
+	for (int i = 0; i < strlen(str); i++)
+	{
+		mas[i] = str[i];
+		if (str[i] == ' ')
+		{
+			if (chekPalindrom(mas))
+				printf("Palindrom");
+			else
+				printf("No palindrom");
+		}
+	}*/
 	#pragma endregion
 
 	#pragma region 19 ЗАДАНИЕ
 	char wrd1[17], wrd2[17], c[17];
-
 	printf("Введите первое слово: ");gets_s(wrd1);
 	printf("Введите второе слово: ");gets_s(wrd2);
 
-	for (int i = 0; wrd1[i] != '\0'; i++)
-	{
-		int counter = 0;
-		for (int j = 0; wrd2[j] != '\0'; j++)
-			if (wrd1[i] == wrd2[j])
-			{
-				counter++;
-				c[i] = wrd1[i];
-			}
-		if (counter == 1)
-			printf("%c\n", c[i]);
-	}
+	recurringSymbol(wrd1, wrd2, c);
 	#pragma endregion
 
 	return 0;
