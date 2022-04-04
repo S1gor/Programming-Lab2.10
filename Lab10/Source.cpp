545556e503dd8154641b5530b31b040838dc82ae
Igor Semikin <i.semikin@list.ru>
2022-04-04 13:09:16 +0300

    Добавил switch

--- Lab10/Source.cpp
+++ Lab10/Source.cpp
@@ -9,7 +9,7 @@ int askUserNumberTask()
 {
 	int choice;
 	do {
-		printf("Введите номер задания (1, 10, 19):\nВыбор:"); scanf_s("%d", &choice);
+		printf("Введите номер задания (1, 10, 19):\nВыбор:"); scanf_s("%d%*c", &choice);
 	} while (choice != 1 && choice != 10 && choice != 19);
 	return choice;
 }
@@ -66,40 +66,45 @@ int main()
 {
 	system("chcp 1251"); system("cls");
 
-	#pragma region 1 ЗАДАНИЕ
-	char str[256];
-	printf("Введите строку:\n"); gets_s(str);
-	char let = choiceLetter();
-
-	letterMet(str, let);
-	#pragma endregion
-
-	#pragma region 10 ЗАДАНИЕ
-	/*char str[256];
-	printf("Введите строку:\n"); gets_s(str);
-
-	char mas[50];
-
-	for (int i = 0; i < strlen(str); i++)
+	switch (askUserNumberTask())
 	{
-		mas[i] = str[i];
-		if (str[i] == ' ')
+	case 1:
+	{
+		char str[256];
+		printf("Введите строку:\n"); gets_s(str);
+		char let = choiceLetter();
+
+		letterMet(str, let);
+		break;
+	}
+	case 10:
+	{
+		char str[256];
+		printf("Введите строку:\n"); gets_s(str);
+
+		char mas[20];
+
+		for (int i = 0; i < strlen(str); i++)
 		{
-			if (chekPalindrom(mas))
-				printf("Palindrom");
-			else
-				printf("No palindrom");
+			mas[i] = str[i];
+				
 		}
-	}*/
-	#pragma endregion
+		if (chekPalindrom(mas))
+			printf("Палиндром\n");
+		else
+			printf("Не палиндром\n");
+		break;
+	}
+	case 19:
+	{
+		char wrd1[17], wrd2[17], c[17];
+		printf("Введите первое слово: "); gets_s(wrd1);
+		printf("Введите второе слово: "); gets_s(wrd2);
 
-	#pragma region 19 ЗАДАНИЕ
-	char wrd1[17], wrd2[17], c[17];
-	printf("Введите первое слово: ");gets_s(wrd1);
-	printf("Введите второе слово: ");gets_s(wrd2);
-
-	recurringSymbol(wrd1, wrd2, c);
-	#pragma endregion
+		recurringSymbol(wrd1, wrd2, c);
+		break;
+	}
+	}
 
 	return 0;
 }
