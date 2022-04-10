3bce9d3f6c1f4659837b8af6526b997c899cb3b6
Igor Semikin <i.semikin@list.ru>
2022-04-10 16:46:23 +0300

    Release

--- Lab10/Source.cpp
+++ Lab10/Source.cpp
@@ -29,23 +29,34 @@ char choiceLetter()
 	scanf_s("%c", &let);
 	return let;
 }
-void letterMet(char* str, char letter)
+char registrReplacement(char letter)
+{
+	if (letter >= 'А' && letter <= 'Я')
+		return letter + ('а' - 'А');
+	else
+		return letter - ('а' - 'А');
+}
+void letterMet(char* str, char letter,char letter2)
 {
 	int counter = 0;
 	int len = strLen(str) + 1;
 
 	for (int i = 0; i < len; i++)
-		if (str[i] == letter)
+		if (str[i] == letter || str[i] == letter2)
 			counter++;
-	printf("Буква '%c' встретилась %d раз(а).", letter, counter);
+	printf("Буква '%c(%c)' встретилась %d раз(а).", letter, letter2, counter);
 }
 
 bool chekPalindrom(char* mas)
 {
 	int len = strlen(mas);
+	char let;
 	for (int i = 0; i < len / 2; i++)
-		if (mas[i] != mas[len - i - 1])
+	{
+		let = registrReplacement(mas[i]);
+		if (mas[i] != mas[len - i - 1] && let != mas[len - i - 1])
 			return false;
+	}
 	return true;
 }
 void findLenAndPalindrom(char* str)
@@ -87,17 +98,19 @@ void findLenAndPalindrom(char* str)
 
 void recurringSymbol(char* wrd1, char* wrd2, char* c)
 {
+	char wrd1_2;
 	for (int i = 0; wrd1[i] != '\0'; i++)
 	{
+		wrd1_2 = registrReplacement(wrd1[i]);
 		int counter = 0;
 		for (int j = 0; wrd1[j] != '\0'; j++)
-			if (wrd1[i] == wrd1[j])
+			if (wrd1[i] == wrd1[j] || wrd1_2 == wrd1[j])
 				counter++;
 		if (counter == 1)
 		{
 			int counter2 = 0;
 			for (int k = 0; wrd2[k] != '\0'; k++)
-				if (wrd1[i] == wrd2[k])
+				if (wrd1[i] == wrd2[k] || wrd1_2 == wrd2[k])
 				{
 					counter2++;
 					c[i] = wrd1[i];
@@ -119,8 +132,9 @@ int main()
 		char str[256];
 		printf("Введите строку:\n"); gets_s(str);
 		char letter = choiceLetter();
+		char letter2 = registrReplacement(letter);
 
-		letterMet(str, letter);
+		letterMet(str, letter, letter2);
 		break;
 	}
 	case 10:
@@ -141,6 +155,5 @@ int main()
 		break;
 	}
 	}
-
 	return 0;
 }
